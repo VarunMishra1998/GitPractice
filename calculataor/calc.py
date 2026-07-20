0b5007769ed4069059faf7b920c05816d0a5d5c4
VarunMishra9838 <varunmishra9838@gmail.com>
2026-07-20 12:02:55 +0530

    add calc.py file

---
+++ calculataor/calc.py
@@ -0,0 +1,11 @@
+a = int(input("enter a first number:"))
+b = int(input("enter a second number:"))
+c = input("enter the operation you want to perform (+, -, *, /):")
+if c == "+":
+    print(a + b)
+elif c == "-":
+    print(a - b)
+elif c == "*":
+    print(a * b)
+elif c == "/":
+    print(a / b)    
